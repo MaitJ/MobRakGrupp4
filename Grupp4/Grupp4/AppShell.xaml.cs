23c5244bbd16f2411023358e23143378d463c72e
Martin Kilgi <martinkl@tlu.ee>
2022-03-11 18:33:02 +0200

    flyout nav

---
+++ Grupp4/Grupp4/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Grupp4
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
