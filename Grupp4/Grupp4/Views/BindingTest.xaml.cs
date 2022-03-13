888f6503c63b99b1b500051a03f5060a38f43e16
MaitJ <MaitJ@tlu.ee>
2022-03-13 09:58:15 +0200

    Tried adding items to flyout menu and tested data binding through code

---
+++ Grupp4/Grupp4/Views/BindingTest.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Grupp4.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BindingTest : ContentPage
+	{
+		public BindingTest ()
+		{
+			InitializeComponent ();
+			label.BindingContext = slider;
+			label.SetBinding(Label.RotationProperty, "Value");
+		}
+	}
+}
