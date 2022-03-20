d7115c3fa9ad76ed2d4e345c8d51bef9d80338e4
Donsirsimon <kenny85@tlu.ee>
2022-03-20 23:05:27 +0200

    added camerapage

---
+++ Grupp4/Grupp4/Views/CameraPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Grupp4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+
+            imgViewPanel.IsVisible = false;
+
+        }
+
+        private void CaptureImage(object sender, EventArgs e)
+        {
+           
+            xctCameraView.Shutter();
+        }
+      
+        private void MediaCaptured(object sender, MediaCapturedEventArgs e)
+        {
+
+            imgView.Source = e.Image;
+            imgViewPanel.IsVisible = true;
+        }
+
+        private void CloseImageView(object sender, EventArgs e)
+        {
+            imgViewPanel.IsVisible = false;
+        }
+    }
+}
