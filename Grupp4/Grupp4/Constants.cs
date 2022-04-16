a7d1efb5ccce0162675fade86ab2e2809cd2feee
Martin Kilgi <martinkl@tlu.ee>
2022-04-16 15:51:26 +0300

    Initial API integration

---
+++ Grupp4/Grupp4/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grupp4
+{
+    public class Constants
+    {
+        public const string GitHubReposEndpoint = "https://api.github.com/orgs/dotnet/repos";
+        public const string WeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat=23.4&lon=15.6&appid=0827dbe14786d005d4c920ab375b9306";
+    }
+}
