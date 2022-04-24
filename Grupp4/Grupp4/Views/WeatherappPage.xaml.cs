4771aec2339a1da5a9f2647b712e0c2948eea5a8
Martin Kilgi <martinkl@tlu.ee>
2022-04-24 15:30:32 +0300

    Gets latitude and longitude and retrieves data according to them

--- Grupp4/Grupp4/Views/WeatherappPage.xaml.cs
+++ Grupp4/Grupp4/Views/WeatherappPage.xaml.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,27 +14,66 @@ namespace Grupp4
     public partial class WeatherappPage : ContentPage
     {
         RestService _restService;
+        CancellationTokenSource cts;
         public WeatherappPage()
         {
             InitializeComponent();
             _restService = new RestService();
+            OnGetWeatherButtonClicked();
         }
-        async void OnGetWeatherButtonClicked(object sender, EventArgs e)
+        async void OnGetWeatherButtonClicked()
         {
-            if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
-            {
-                WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.WeatherEndpoint));
+            
+                WeatherData weatherData = await _restService.GetWeatherData(await GetCurrentLocation(Constants.WeatherEndpoint));
                 BindingContext = weatherData;
+            
+        }
+        async Task<string> GetCurrentLocation(string endpoint)
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+                string requestUri = endpoint;
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}");
+                    requestUri += $"?lat={location.Latitude}";
+                    requestUri += $"&lon={location.Longitude}";
+                    requestUri += "&units=metric";
+                    requestUri += $"&APPID={Constants.WeatherAPIKey}";
+                }
+                return requestUri;
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+                return null;
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+                return null;
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+                return null;
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+                return null;
             }
         }
 
-        string GenerateRequestUri(string endpoint)
+        protected override void OnDisappearing()
         {
-            string requestUri = endpoint;
-            requestUri += $"?q={_cityEntry.Text}";
-            requestUri += "&units=metric";
-            requestUri += $"&APPID={Constants.WeatherAPIKey}";
-            return requestUri;
+            if (cts != null && !cts.IsCancellationRequested)
+                cts.Cancel();
+            base.OnDisappearing();
         }
     }
 }
